b5e5a00ae72160c2f74e754db0f69eea41560c6a
Anish8825 <anishmurthi8825@gmail.com>
2026-08-23 21:38:14 +0530

    variable cant be changed

--- index.js
+++ index.js
@@ -69,16 +69,44 @@ username = window.prompt("What is your username");
 
 console.log(username);
 */
-let username;
+/*let username;
 
 document.getElementById("mysubmit").onclick = function(){
   username = document.getElementById("mytext").value;
   document.getElementById("myuu").textContent = `Hello ${username}`;
 }
+  */
 //------------------------------------------------------------//
 
-let age = window.prompt("How old are you?");
+/*
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
 
-age+=1;
+x = Number(x);
+y = String(y);
+z = Boolean(z);
 
-console.log(age);
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+*/
+//-------------------------------------------------------------//
+
+
+//const = a variable that can't be changed//
+/*
+const PI = 3.14159;
+let radius;
+let circumference;
+
+
+document.getElementById("mySubmit").onclick = function(){
+  radius = document.getElementById("mytext").value;
+  radius = Number(radius);
+  circumference = 2 * PI * radius;
+  document.getElementById("myH3").textContent = circumference + "cm";
+}
+*/
+
+//-------------------------------------------------------------//
