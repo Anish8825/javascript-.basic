32e745e6a534ba43724725734e29f6e0692b5401
Anish8825 <anishmurthi8825@gmail.com>
2026-08-29 22:24:24 +0530

    number guessing game

--- index.js
+++ index.js
@@ -677,3 +677,45 @@ for(let i = 1; i <= 20; i ++){
   }
 }
 */
+
+//-------------------------------------------------------------------//
+
+//NUMBER GUESSING GAME//
+/*
+const minNum = 1;   // constant minimum number//
+const maxNum = 100;  // constant maximum number//
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+              //in this random number is choosed. maxnum is subtracted by minnum and 1 is added and 
+              //random num and calculated num is multiplied and atlast minnum is added//
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while(running){
+
+  guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`); //gives alert prompt on top of the window//
+  guess = Number(guess); 
+  
+  if(isNaN(guess)){
+    window.alert("Please enter a validate number"); //alert prompt//
+  }
+  else if(guess < minNum || guess > maxNum){ 
+    window.alert("Please enter a valid number");
+  }
+  else{
+    attempts++;
+    if(guess < answer){
+      window.alert("TOO LOW! TRY AGAIN!");
+    }
+    else if(guess > answer){
+      window.alert("TOO HIGH! TRY AGAIN!");
+    }
+    else{
+      window.alert(`CORRECT ! THE ANSWER WAS ${answer}. IT TOOK YOU ${attempts} ATTEMPTS`);
+      running = false;
+    }
+  }
+
+}
+*/
