96f735fe647e95a5c75a7ef2d3df7cf39ff784e7
Anish8825 <anishmurthi8825@gmail.com>
2026-09-04 15:33:03 +0530

    Arrow function

--- WEB MAIN/index.js
+++ WEB MAIN/index.js
@@ -1139,3 +1139,33 @@ console.log(evenNums);
 console.log(total);        */
 
 //=====================================================================================================//
+
+//Arrow functions = A concise way to write function expressions 
+//                  good for simple function that you use only once (parameters) = some code
+
+/*
+const hello = () => console.log("HELLO WORLD");
+
+hello();   */
+/*
+const hello = (name) => console.log(`HELLO ${name}`);
+
+hello("ANISH");   */
+
+//EXAMPLE:
+/*
+const numbers = [1,2,3,4,5,6];
+
+const square = numbers.map((element) => Math.pow(element, 2));
+const cubes = numbers.map((element) => Math.pow(element, 3));
+const evenNumbs = numbers.filter((element) => element %2 === 0);
+const oddNumbs = numbers.filter((element) => element !== 0);
+const total = numbers.reduce((accumulator, element) => accumulator+element);
+
+console.log(square);
+console.log(cubes);
+console.log(evenNumbs);
+console.log(oddNumbs);
+console.log(total); */
+
+//=========================================================================================================//
