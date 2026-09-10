0083fcf20a5c0bdb418b95c276af215c12dbc28a
Anish8825 <anishmurthi8825@gmail.com>
2026-09-10 20:03:57 +0530

    Digital clock program

--- index.js
+++ index.js
@@ -1906,3 +1906,28 @@ function cleartimer(){
 
 
 //======================================================================================================// 
+
+//------DIGITAL CLOCK PROGRAM------//
+
+/*
+
+function updateclock(){
+
+  const now = new Date();
+  let hours = now.getHours();
+  const meridiem = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+  hours = hours.toString().padStart(2,0)
+  const minutes = now.getMinutes().toString().padStart(2,0);
+  const seconds = now.getSeconds().toString().padStart(2,0);
+  const timestring = `${hours}:${minutes}:${seconds} ${meridiem}`;
+
+  document.getElementById("clock").textContent = timestring;
+
+}
+updateclock();
+setInterval(updateclock,100);
+
+*/
+//===========================================================================================================//
+
