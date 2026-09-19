8f8eb262688cefba04fb4902d3ad94422bda3f32
Anish8825 <anishmurthi8825@gmail.com>
2026-09-19 10:27:53 +0530

    example of key eventlistener

--- index.js
+++ index.js
@@ -2573,3 +2573,49 @@ document.addEventListener("keyup", event => {
 });
 */
 
+//EXAMPLE:
+const mybox = document.getElementById("mybox");
+const moveAmount = 10;
+let x = 0;
+let y = 0;
+
+document.addEventListener("keydown", event => {
+  mybox.textContent = "😉";
+  mybox.style.backgroundColor = "blue";
+});
+
+document.addEventListener("keyup", event => {
+  mybox.textContent = "💕";
+  mybox.style.backgroundColor = "pink";
+});
+
+document.addEventListener("keydown", event => {
+
+
+  if(event.key.startsWith("Arrow")){
+
+    event.preventDefault();
+
+    switch(event.key){
+      case "ArrowUp":
+        y -= moveAmount;
+        break;
+
+      case "ArrowDown":
+        y += moveAmount;
+        break;
+
+      case "ArrowLeft":
+        x -= moveAmount;
+        break;
+      
+      case "ArrowRight":
+        x += moveAmount;
+        break;
+    }
+
+    mybox.style.top = `${y}px`;
+    mybox.style.left = `${x}px`;
+
+  }
+});
